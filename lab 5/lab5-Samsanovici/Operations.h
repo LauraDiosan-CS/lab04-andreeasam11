787b9ca27dc7fef946f5880c2f09890b074d97f5
Andreea Samsanovici <s_andreea00@yahoo.com>
2020-03-25 10:14:30 +0200

    am pus fisierele de la lab  4 intr un folder si am adaugat lab 5

--- lab 5/lab5-Samsanovici/Operations.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "Project.h"
-#include "Repository.h"
-
-/*void filterProjects(Project projects[], int n, int k, int l, Project result[], int& resultLen);
-void filterProjectsWithRepository(Repository repo, int n, int k, int l, Project result[], int& resultLen);
-void elimProjects(Project projects[], int n, Project result[], int& resultLen);
-void elimProjectsWithRepository(Repository repo, int n, Project result[], int& resultLen);*/
--- lab 5/lab5-Samsanovici/RepoTeste.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-void testAdd();
-void testGetAll();
-void testeConstructor();
-void testDelete();
-void testUpdate();
